bbb6d8cb0be0366558e3b53f096b0f8214e88a23
Dami <85047035+dami0806@users.noreply.github.com>
2022-11-13 19:08:51 +0900

    Add files via upload

---
+++ ToseeLifecycle/ToseeLifecycle/SecondViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SecondViewController.swift
+//  ToseeLifecycle
+//
+//  Created by 박다미 on 2022/11/13.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("****VC2 구현")
+        
+    }
+
+    
+    
+    //뷰 나타나기 전
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("****VC2 viewWillAppear 호출")
+    }
+    
+    
+    //뷰 나타나기 후
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("****VC2 viewDidAppear 호출")
+    }
+    //뷰 사라지기 전
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("****VC2 viewWillDisappear 호출")
+    }
+    
+    //뷰 사라지기 후
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("****VC2 viewDidDisappear 호출")
+    
+    }
+    
+
+    
+    @IBAction func BackTappedBtn(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+  // test
+    
+    
+    deinit{
+        print("****VC2 메모리에서 내려감")
+    }
+    
+
+}
